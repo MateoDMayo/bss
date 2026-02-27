17559dd4e5316e9f18dfe3d7951145f23c2aff0e
MateoDMayo <carstensenmateo@gmail.com>
2026-02-26 20:07:12 -0600

    mapMesh.js

--- mapMesh.js
+++ mapMesh.js
@@ -283,7 +283,9 @@ box(-17,-1,-19,15,4,15,[0,25+45,0],[0.6,0,0],true,false,true);
 
 window.stuffColorShiftedForBeesmas=1
 
-box(0, 200, 0, 20, 20, 20, false, [0.6, 0.6, 0.6], true, false);
+// DEBUG MEGA MARKERS
+box(0, 1500, 0, 800, 800, 800, false, [0.6, 0.6, 0.6], true, false);
+box(0, 200,  0, 2000, 50, 2000, false, [0.7, 0.7, 0.7], true, false); // giant “ceiling slab”
 
 box(0,-5,0,59.9,7,100,false,[0.2,0.8,0.2],true,false);
 box(0,-5,26,170,7,70,false,[0.2,0.8,0.2],true,false);
